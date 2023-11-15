a789a23bdd9465822c0ceb39848f8fab9568058e
JasemDuncan <jasemvalencia@gmail.com>
2023-11-15 06:05:11 -0500

    add userList component

--- ReactTypeScript55k/src/App.tsx
+++ ReactTypeScript55k/src/App.tsx
@@ -5,6 +5,18 @@ import { UsersList } from './components/UsersList'
 
 function App() {
   const [users, setUsers] =useState<User[]>([])
+  const [showColors, setSjowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
+
+  const toogleColors = () => {
+    setSjowColors(!showColors)
+  }
+
+  const toggleSortByCountry = () => {
+    setSortByCountry(prev=> !prev)
+  }
+
+
   useEffect(()=>{
     fetch('https://randomuser.me/api?results=100')
     .then(res => res.json())
@@ -16,10 +28,30 @@ function App() {
     })
   },[])
 
+  const sortedUsers = sortByCountry
+    ? [...users].sort((a,b) => { 
+    return a.location.country.localeCompare(b.location.country)
+  })
+  :
+  users
+
   return (
     <div>
       <h1>Test</h1>
-      <UsersList users={users} />
+      <header>
+        <button onClick={toogleColors}>
+          Toogle colors
+        </button>
+        <button onClick={toggleSortByCountry}>
+          {
+            sortByCountry ? 'Not order by country': 'Order by country'
+          }
+        </button>
+      </header>
+      <main>
+        <UsersList showColors={showColors} users={users} />
+      </main>
+      
     </div>
   )
 }
